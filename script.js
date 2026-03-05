94155d354309d809d55261d7bb710f59d7677ea2
1301318619-ctrl <1301318619@s.tusd1.org>
2026-03-05 21:06:17 +0000

    done

--- script.js
+++ script.js
@@ -3,8 +3,14 @@ console.log("Script started");
 // Variable to track the number of clicks
 let clicks = 0;
 
+// Click value
+let clickValue = 1;
+
 // Variable to store the click display element
 let clickDisplay = document.getElementById("click-display");
+
+// Variable for level display
+let levelDisplay = document.getElementById("level-display");
 
 // Set the initial of click display
 clickDisplay.innerText = "Clicks: " + clicks;
@@ -14,12 +20,14 @@ function handleClick() {
     console.log("click");
 
    // Count the click
-   clicks = clicks + 1;
+   clicks = clicks + clickValue;
 
    // Update display with new value
    clickDisplay.innerText = "Clicks: " + clicks;
 
    if (clicks == 10) {
-        alert("Congratulations you reached 10 clicks, you're at level 2!")
+        alert("Congrats! You've reached level 2!");
+        levelDisplay.innerText = "Level 2";
+        clickValue = 2;
    }
 }
